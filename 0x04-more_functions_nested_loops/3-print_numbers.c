61dc9309f4650361aef1ab5814f014f674f4941e
shrook <shrookalattar8@gmail.com>
2023-06-22 07:43:51 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_number - print 0 - 9
+ * print_numbers - print 0 - 9
  * only using _putchar twice
  *
- * Return" Always 0 (success)
+ * Return: Always 0 (success)
 */
 
 void print_numbers(void)
