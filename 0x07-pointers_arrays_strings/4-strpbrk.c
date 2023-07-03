35d4309de08ca8d9f450ec6fac8a06502972cc45
shrook <shrookalattar8@gmail.com>
2023-07-03 06:19:00 -0700

    a

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,8 +2,8 @@
 
 /**
  * _strpbrk - Search a string for any of a set of bytes.
- * @s: string
- * @accept: string to match
+ * @s: string.
+ * @accept: string to match.
  * Return: pointer to the byte in s that matches one of the bytes in
  * accpt or NULL if no such byte is found.
 */
@@ -13,7 +13,7 @@ char *_strpbrk(char *s, char *accept)
 	int itr, jtr;
 	char *p;
 
-	itr - 0;
+	itr = 0;
 	while (s[itr] != '\0')
 	{
 		jtr = 0;
@@ -21,7 +21,7 @@ char *_strpbrk(char *s, char *accept)
 		{
 			if (accept[jtr] == s[itr])
 			{
-				p = $s[itr];
+				p = &s[itr];
 				return (p);
 			}
 			jtr++;
