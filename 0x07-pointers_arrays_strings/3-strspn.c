12870453a53fabaf1e1974bd2342c5823527fe59
shrook <shrookalattar8@gmail.com>
2023-07-03 06:03:44 -0700

    a

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,9 +3,9 @@
 /**
  * _strspn - gets the length of a prefix substring.
  *
- * @s: string
- * @accept: bytes
- * Return: unsigned int
+ * @s: string.
+ * @accept: bytes.
+ * Return: unsigned int.
 */
 
 unsigned int _strspn(char *s, char *accept)
