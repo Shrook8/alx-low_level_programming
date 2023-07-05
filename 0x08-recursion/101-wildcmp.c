aadae14c8138bd03b6455619b909eb39561697f1
shrook <shrookalattar8@gmail.com>
2023-07-05 16:50:20 -0700

    a

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -51,11 +51,10 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s1 == *s2)
 	{
-		if (!*s1 == *s2)
-			if (!*s1)
-				return (1);
-			return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
-		}
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
 		if (!*s1 || !s2)
 			return (0);
 		if (*s2 == '*')
