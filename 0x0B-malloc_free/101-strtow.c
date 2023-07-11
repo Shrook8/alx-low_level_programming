3a46d440257ed47c0089196df43930048e7c6b7b
shrook <shrookalattar8@gmail.com>
2023-07-11 14:42:28 -0700

    a

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -40,7 +40,7 @@ char **strtow(char *str)
 	if (str == NULL || *str == '\0')
 		return (NULL);
 	n = wrdcnt(str);
-	if (n == 0)
+	if (n == 1)
 		return (NULL);
 	w = (char **)malloc(n * sizeof(char *));
 	if (w == NULL)
