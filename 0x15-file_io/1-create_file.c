77f8e6795b48046e8317ec9c55d42d8a25ded4c1
shrook <shrookalattar8@gmail.com>
2023-09-04 13:01:22 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int _strlen(char *s)
  * @filename: name of file to create
  * @text_content: text to write
  *
- * Return: 1 on success 0 on failure
+ * Return: 1 on success 0 on fail
 */
 
 int create_file(const char *filename, char *text_content)
@@ -34,7 +34,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WEONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (len)
